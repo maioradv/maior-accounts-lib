d79cddd539d9012b1d9d279b70ef9e2151cbe570
Roberto Colabene <robertocolabene@maior.team>
2025-10-21 10:52:29 +0200

    dev

--- src/auth/apitoken.ts
+++ src/auth/apitoken.ts
@@ -2,7 +2,7 @@ import { ApiModule } from "../model";
 import { AccessTokenDto } from "./types";
 
 export default class AuthApiToken extends ApiModule {
-  token(apiToken:string): Promise<AccessTokenDto> {
-    return this._call<AccessTokenDto>('post',`/auth/token/${apiToken}`)
+  token(token:string): Promise<AccessTokenDto> {
+    return this._call<AccessTokenDto>('post',`/auth/token`,{token})
   }
 }
