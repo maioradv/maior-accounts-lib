1297341b0116ac5a0a82b0e89a50ba391059aabf
Roberto Colabene <robertocolabene@maior.team>
2025-12-04 10:37:43 +0100

    dev

--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,5 @@ export function SlugTransform(str:string) {
     .normalize("NFD").replace(/[\u0300-\u036f]/g, '')                 // no accent
     .replace(/[^a-z0-9\s]/gi, '')                                     // no special except space
     .replaceAll(' ','-') 
+    .toLowerCase()
 }
