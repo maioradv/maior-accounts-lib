099b8d0a121130ad90501a4c12c743aa165907ad
Sabatino Carotenuto <sabatinocarotenuto@maioradv.com>
2026-05-08 12:44:22 +0200

    dashboardTypeSlug

--- src/dashboards/types.ts
+++ src/dashboards/types.ts
@@ -57,6 +57,7 @@ export type ClausesDashboardDto = WhereClausesDto<{
   plan?:StringClause,
   status?:EnumClause<DashboardStatus>,
   search?:StringClause,
+  dashboardTypeSlug?:StringClause,
 }>
 
 export type QueryDashboardDto = QueryParamsDto<SortingDashboardDto,ClausesDashboardDto>
